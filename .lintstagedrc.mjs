48a05de4c240af6808918527b005d79fce106fe9
Abdullah bin Kasim <abdullah.kasim@automattic.com>
2023-01-11 11:23:42 +0800

    Update prettier's config

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,6 +1,6 @@
 const config = {
 	'*.{js,jsx,ts,tsx}': ["npx prettier --write", "npx eslint --fix --quiet"],
-	'*.{md,html,css,scss,sass}': ["npx prettier --write"]
+	'*.{md,html,css,scss,sass,yaml,yml}': ["npx prettier --write"]
 }
 
 export default config
