c2ebaf876c6937746d320cd87b6009e3c85ce0c8
Abdullah bin Kasim <abdullah.kasim@automattic.com>
2023-02-10 17:07:58 +0800

    Add some skeleton for autoMerge tests

---
+++ dependabot-auto-merge/__tests__/autoMerge.ts
@@ -0,0 +1,27 @@
+describe( 'autoMerge', () => {
+	describe( 'isVersionBumpSafeToMerge', () => {
+		// noop
+	} );
+
+	describe( 'checkPullRequestApprovable', () => {
+		// noop
+	} );
+
+	describe( 'isPullRequestMergeable', () => {
+		// noop
+	} );
+
+	describe( 'markAutoMergePullRequest', () => {
+		// noop
+	} );
+
+	describe( 'mergePullRequestsInRepository', () => {
+		// noop
+	} );
+
+	describe( 'mergeDependabotPullRequests', () => {
+		// noop
+	} );
+} );
+
+export {};
