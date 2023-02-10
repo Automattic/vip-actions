350b5c7636b80ca086c5422f441d7b16b945a4f5
Abdullah bin Kasim <abdullah.kasim@automattic.com>
2023-02-10 17:25:19 +0800

    Add tests for testing isVersionBumpSafeToMerge

--- dependabot-auto-merge/__tests__/autoMerge.ts
+++ dependabot-auto-merge/__tests__/autoMerge.ts
@@ -1,6 +1,36 @@
+import {
+	mergeableDescriptions,
+	unmergeableDescriptions,
+} from '../__fixtures__/autoMerge/autoMerge';
+import { isVersionBumpSafeToMerge } from '../src/autoMerge';
+
 describe( 'autoMerge', () => {
 	describe( 'isVersionBumpSafeToMerge', () => {
-		// noop
+		it.each( mergeableDescriptions )(
+			'should return true if the PR description has safe version bumps',
+			( description: string ) => {
+				expect( isVersionBumpSafeToMerge( description ) ).toBe( true );
+			}
+		);
+
+		it.each( unmergeableDescriptions )(
+			'should return false if the PR description has unsafe version bumps',
+			( description: string ) => {
+				expect( isVersionBumpSafeToMerge( description ) ).toBe( false );
+			}
+		);
+
+		it.each( [
+			{
+				description: '',
+				type: 'an empty string',
+			},
+		] )(
+			'should return false for the edge case where the description is $type',
+			( { description } ) => {
+				expect( isVersionBumpSafeToMerge( description ) ).toBe( false );
+			}
+		);
 	} );
 
 	describe( 'checkPullRequestApprovable', () => {
