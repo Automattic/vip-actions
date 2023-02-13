7191e98ea7f58b7080859534c791dc88d54f42a8
Abdullah bin Kasim <abdullah.kasim@automattic.com>
2023-02-13 17:58:48 +0800

    Jest can't properly partially mock a module, so we add a plugin to fix that
    See https://github.com/Xunnamius/babel-plugin-explicit-exports-references

--- dependabot-auto-merge/babel.config.js
+++ dependabot-auto-merge/babel.config.js
@@ -4,6 +4,15 @@ const config = {
 		[ '@babel/preset-env', { targets: { node: 'current' } } ],
 		'@babel/preset-typescript',
 	],
+	env: {
+		test: {
+			plugins: [
+				// see https://github.com/facebook/jest/issues/936#issuecomment-821944391
+				// Jest has no good way of partially mocking a module
+				'explicit-exports-references',
+			],
+		},
+	},
 };
 
 module.exports = config;
