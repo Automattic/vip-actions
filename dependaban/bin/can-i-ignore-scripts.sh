7b7495571985bbda2062ad4f6f34fb50f1bc09a5
chriszarate <chris.zarate@automattic.com>
2023-02-23 18:09:21 -0500

    Add Dependaban action to flag dependencies with install scripts

---
+++ dependaban/bin/can-i-ignore-scripts.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Print output and save to variable for inspection. We need to inspect because
+# it always exits with zero.
+# https://github.com/naugtur/can-i-ignore-scripts/pull/25
+CHECK_OUTPUT="$(npx can-i-ignore-scripts | tee /dev/stderr)"
+
+if printf "%s" "$CHECK_OUTPUT" | grep -qE '^\[ +(keep|check\?) +\]'; then
+	echo "Non-ignorable install scripts found!"
+	exit 1
+fi
