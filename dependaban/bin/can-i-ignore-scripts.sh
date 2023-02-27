6909b29eac9ee477d045dbe45f982df8f719dc22
chriszarate <chris.zarate@automattic.com>
2023-02-27 10:08:07 -0500

    Target specific version

--- dependaban/bin/can-i-ignore-scripts.sh
+++ dependaban/bin/can-i-ignore-scripts.sh
@@ -3,7 +3,7 @@
 # Print output and save to variable for inspection. We need to inspect because
 # it always exits with zero.
 # https://github.com/naugtur/can-i-ignore-scripts/pull/25
-CHECK_OUTPUT="$(npx can-i-ignore-scripts | tee /dev/stderr)"
+CHECK_OUTPUT="$(npx can-i-ignore-scripts@0.1.9 | tee /dev/stderr)"
 
 if printf "%s" "$CHECK_OUTPUT" | grep -qE '^\[ +(keep|check\?) +\]'; then
 	echo "Non-ignorable install scripts found!"
